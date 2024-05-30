589281d494d56835d146796d1b41ccb856177f64
ChandragiriGouthamSai <120169641+ChandragiriGouthamSai@users.noreply.github.com>
2024-05-30 14:14:41 +0530

    goutham 30-05-2024 14:14

--- services/subscriptionServices.js
+++ services/subscriptionServices.js
@@ -14,52 +14,100 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
+// const createSubscription = async (request, response) => {
+//     try {
+//         const {
+//             user_id,
+//             subscription_price,
+//             purchased_date,
+//             expired_date,
+//             subscription_type
+//         } = request.body;
+
+      
+//         const purchasedDateParts = purchased_date.split('-');
+//         const expiredDateParts = expired_date.split('-');
+
+//         const purchasedDate = new Date(`${purchasedDateParts[2]}-${purchasedDateParts[1]}-${purchasedDateParts[0]}`);
+//         const expiredDate = new Date(`${expiredDateParts[2]}-${expiredDateParts[1]}-${expiredDateParts[0]}`);
+
+       
+//         const validThroughDays = Math.floor((expiredDate - purchasedDate) / (1000 * 60 * 60 * 24));
+
+        
+//         const validThroughInterval = `${validThroughDays} days`;
+
+//         const insertQuery = `
+//             INSERT INTO subscription (user_id, subscription_price, purchased_date, expired_date, valid_through, subscription_type)
+//             VALUES ($1, $2, $3, $4, $5, $6)
+//         `;
+
+//         await pool.query(insertQuery, [
+//             user_id,
+//             subscription_price,
+//             purchasedDate.toISOString().split('T')[0], 
+//             expiredDate.toISOString().split('T')[0], 
+//             validThroughInterval,
+//             subscription_type
+//         ]);
+
+//         response.status(200).json({ message: 'Subscription created successfully' });
+//     } catch (error) {
+//         console.error('Error executing query', error);
+//         response.status(500).json({ error: 'Internal Server Error' });
+//     }
+// };
 const createSubscription = async (request, response) => {
     try {
         const {
             user_id,
             subscription_price,
-            purchased_date,
-            expired_date,
             subscription_type
         } = request.body;
 
-      
-        const purchasedDateParts = purchased_date.split('-');
-        const expiredDateParts = expired_date.split('-');
+        let nextMonthDate = new Date(); 
 
-        const purchasedDate = new Date(`${purchasedDateParts[2]}-${purchasedDateParts[1]}-${purchasedDateParts[0]}`);
-        const expiredDate = new Date(`${expiredDateParts[2]}-${expiredDateParts[1]}-${expiredDateParts[0]}`);
+        if (subscription_type === "9909ce77-02fe-49a5-840f-dad31e903a56") {
+            nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
+        } else if (subscription_type === "7264f7c3-73b2-41c1-b0bc-83c28e19e97f") {
+            nextMonthDate.setMonth(nextMonthDate.getMonth() + 6);
+        } else if (subscription_type === "e3f1d7db-fe68-4b82-8d02-6d7b9cd7f26d") {
+            nextMonthDate.setMonth(nextMonthDate.getMonth() + 9);
+        } else if (subscription_type === "df8acba7-11bb-494d-b238-815c63ed4d33") {
+            nextMonthDate.setMonth(nextMonthDate.getMonth() + 12);
+        }
 
-       
+        const currentDate = new Date();
+        const purchasedDate = currentDate; 
+        const expiredDate = nextMonthDate; 
+
         const validThroughDays = Math.floor((expiredDate - purchasedDate) / (1000 * 60 * 60 * 24));
-
-        
         const validThroughInterval = `${validThroughDays} days`;
 
         const insertQuery = `
-            INSERT INTO subscription (user_id, subscription_price, purchased_date, expired_date, valid_through, subscription_type)
-            VALUES ($1, $2, $3, $4, $5, $6)
+            INSERT INTO subscription_details (user_id, subscription_price, purchased_date, expired_date, subscription_type)
+            VALUES ($1, $2, $3, $4, $5)
         `;
 
         await pool.query(insertQuery, [
             user_id,
             subscription_price,
-            purchasedDate.toISOString().split('T')[0], 
-            expiredDate.toISOString().split('T')[0], 
-            validThroughInterval,
+            purchasedDate.toISOString().split('T')[0],
+            expiredDate.toISOString().split('T')[0],
             subscription_type
         ]);
+        const token = jwt.sign({ userId: user_id }, 'your_secret_key', { expiresIn: `${validThroughDays}d` });
 
-        response.status(200).json({ message: 'Subscription created successfully' });
+        response.status(200).json({ message: 'Subscription created successfully',token:token });
     } catch (error) {
         console.error('Error executing query', error);
         response.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
 const getSubscription = async (request, response) => {
     try {
-        const subscriptionItems = await pool.query('SELECT * FROM subscription');
+        const subscriptionItems = await pool.query('SELECT * FROM subscription_details');
 
         if (subscriptionItems.rows.length === 0) {
             return response.status(404).json({ error: 'No subscriptions' });
@@ -75,7 +123,7 @@ const deleteSubscription = async (request, response) => {
     try {
         const subscriptionId = request.params.subscription_id;
         const deleteAddressQuery = await pool.query(
-            'DELETE FROM subscription WHERE id = $1',
+            'DELETE FROM subscription_details WHERE id = $1',
             [subscriptionId]
         );
 
