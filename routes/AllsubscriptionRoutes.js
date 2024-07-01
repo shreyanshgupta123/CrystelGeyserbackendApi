26b0649c0f68e92d9d2c9901f012641ee8cf7330
ChandragiriGouthamSai <120169641+ChandragiriGouthamSai@users.noreply.github.com>
2024-07-01 16:41:26 +0530

    goutham 01-07-2024 16:41

--- routes/AllsubscriptionRoutes.js
+++ routes/AllsubscriptionRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
    
-    const {createSubscription,getAllSubscription,updateSubscription,getAllSubscriptionById} = require('../services/AllsubscriptionsServices');
+    const {createSubscription,getAllSubscription,updateAllSubscription,getAllSubscriptionById} = require('../services/AllsubscriptionsServices');
 
 
 router.post('/', createSubscription);
- router.get('/',  getAllSubscription);
- router.get('/:subscription_id', getAllSubscriptionById);
-// router.delete('/:address_id', deleteAlternateAddress);
- router.put('/:subscription_id', updateSubscription);
+router.put('/:subscription_id', updateAllSubscription);
+router.get('/',  getAllSubscription);
+
+router.get('/:subscription_id', getAllSubscriptionById);
+
 
 
 
--- services/AllsubscriptionsServices.js
+++ services/AllsubscriptionsServices.js
@@ -66,34 +66,36 @@ const getAllSubscriptionById = async (request, response) => {
         response.status(500).json({ error: 'Internal Server Error' });
     }
 };
-const updateSubscription = async (request, response) => {
+const updateAllSubscription = async (request, response) => {
     try {
-        const { order_id } = request.params.subscription_id;
+        const order_id = request.params.subscription_id;
         const {
             active_subscription_id,
             cancelled_subscription_id,
-            paused_subscription_id
+            paused_subscription_id,
+            user_id
         } = request.body;
 
         const existingProduct = await pool.query(
             'SELECT * FROM subscription_table WHERE id = $1',
             [order_id]
         );
-
+        // console.log(existingProduct)
         if (existingProduct.rows.length === 0) {
             return response.status(404).json({ error: 'subscription not found' });
         }
 
         const updateQuery = `
             UPDATE subscription_table
-            SET active_subscription_id = $1, cancelled_subscription_id = $2,paused_subscription_id=$3
-            WHERE id = $4
+            SET active_subscription_id = $1, cancelled_subscription_id = $2,paused_subscription_id=$3,user_id=$4
+            WHERE id = $5
         `;
 
         const values = [
             active_subscription_id !== undefined ? active_subscription_id : existingProduct.rows[0].active_subscription_id,
             cancelled_subscription_id !== undefined ? cancelled_subscription_id : existingProduct.rows[0].cancelled_subscription_id,
             paused_subscription_id !== undefined ? paused_subscription_id : existingProduct.rows[0].paused_subscription_id,
+            user_id,
             order_id
         ];
 
@@ -109,6 +111,6 @@ const updateSubscription = async (request, response) => {
 module.exports = {
     createSubscription,
     getAllSubscription,
-    updateSubscription,
+    updateAllSubscription,
     getAllSubscriptionById
 };
